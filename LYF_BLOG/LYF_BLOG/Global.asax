ba3aeac3c24beaf2f235824e632d7908ee524dad
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-20 22:51:41 +1000

    Initiate LYF_BLOG Project

---
+++ LYF_BLOG/LYF_BLOG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LYF_BLOG.MvcApplication" Language="C#" %>
---
+++ LYF_BLOG/LYF_BLOG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LYF_BLOG.Startup))]
+namespace LYF_BLOG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
